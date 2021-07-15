a106422d84abbcfa1f76e8ba6ba82d1554809ff4
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-07-15 17:08:18 -0300

    Adicionada a funcionalidade de renderização de listas com base nos dados do localStorage

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
-export default function Dashboard(){
-  return(
+import List from '../../components/list/list';
+import './dashboard.scss'
+
+export default function Dashboard() {
+  let db: any = JSON.parse(localStorage.getItem('db'));
+  let listas: any = db.lists;
+  return (
+
     <div className="dashboard">
       <h1>Dashboard</h1>
+      <div className="list-container">
+        {listas.map((list: any, index: any) =>
+          <List title={list.list_title} color={list.list_color} key={index} />
+        )}
+        <div className="add-list-button">
+          <i className='bx bx-plus-circle'></i>
+        </div>
+      </div>
     </div>
   );
 }
