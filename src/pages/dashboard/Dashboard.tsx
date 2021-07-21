4e0ac26c944c38a79165a06fb101abc9df33c59b
G4BR-13-L <gabriel.victorc13@gmail.com>
2021-07-20 21:38:38 -0300

    feat: animação de listas na pagina do Dashboard
    Utilizando a biblioteca framer-motion foram feitas animações de fade-in na pagina de dash board.
    Além disso uma pequena modificação funcional foi feita no botão de criação de listas. Agora, inves
    de utilizar um componente [Link], é utuilizado a api de useHistory com push da rota desejada.

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,31 +2,63 @@ import * as React from 'react';
 import List from '../../components/list/list';
 import './dashboard.scss'
 import { Redirect, useHistory, Link } from 'react-router-dom';
+import { motion } from "framer-motion"
 
 export default function Dashboard() {
-
+  const handleButtonClick = (event: any) => {
+    history.push('/newlist')
+  }
   const [db, setDb] = React.useState(JSON.parse(localStorage.getItem('db')))
   const updateDashboard = () => setDb(JSON.parse(localStorage.getItem('db')))
   let listas: any = db.lists;
-  let history:any = useHistory();
+  let history: any = useHistory();
+  const variants = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2
+      }
+    }
+  }
+  const animateList = {
+    hidden: {
+      opacity: 0,
+      y: -80
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8
+      }
+    }
+  }
   return (
-
     <div className="dashboard">
       <h1>Dashboard</h1>
-      <div className="list-container">
+      <motion.div
+        className="list-container"
+        variants={variants}
+        initial="hidden"
+        animate="show"
+      >
         {listas.map((list: any, index: any) =>
-          <List 
-          title={list.list_title} 
-          color={list.list_color} 
-          listId={list.list_id}
-          updateDashboard={updateDashboard} 
-          key={index} />
+          <motion.div variants={animateList}>
+
+            <List
+              title={list.list_title}
+              color={list.list_color}
+              listId={list.list_id}
+              updateDashboard={updateDashboard}
+              key={index} />
+          </motion.div>
         )}
 
-        <Link className="add-list-button" to="/newlist">
+        <motion.div variants={animateList} className="add-list-button" onClick={handleButtonClick}>
           <i className='bx bx-plus-circle'></i>
-        </Link>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
